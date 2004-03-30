7e150922bcdabb201d84471a63654396b078ee21
looksirdroids <looksirdroids>
2004-03-30 21:52:23 +0000

    The other libraries should not be needed as default in benchmarks

--- benchmarks/performance.cc
+++ benchmarks/performance.cc
@@ -19,8 +19,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 //uncomment lines below to benchmark the libraries
 
-#define JNEURAL
-#define LWNN
+//#define JNEURAL
+//#define LWNN
 
 #include <stdio.h>
 #include <stdlib.h>
